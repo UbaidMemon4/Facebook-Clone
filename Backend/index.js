85ddb5cc4b0d5affbd4dca98beb9e8d4ccdc1d8a
UbaidMemon4 <Ubaidasif510@gmail.com>
2024-07-26 14:01:21 -0700

    vercel config

--- Backend/index.js
+++ Backend/index.js
@@ -20,25 +20,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-//cors config
-const corsOptions = {
-  origin: "https://facebook-clone-blue-three.vercel.app/", // Replace with your Vercel app URL
-  optionsSuccessStatus: 200,
-};
+// //cors config
+// const corsOptions = {
+//   origin: "https://facebook-clone-blue-three.vercel.app/", // Replace with your Vercel app URL
+//   optionsSuccessStatus: 200,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 //routes
 app.use("/user", userRoutes);
 app.use("/blog", blogRoutes);
 
 //Port
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send(`<h1>Server Running On Port ${port}</h1>`);
 });
 //listen
-app.listen(3001, () => {
+app.listen(port, () => {
   console.log(`Server Running On Port ${port}`);
 });
